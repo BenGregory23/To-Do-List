bfdb0386161dad5c6aa35e9024a6e43ea33ccff3
ZartaX0O3 <arthur.avd69@gmail.com>
2021-11-24 11:29:12 +0100

    ajout d'une vue + config.php

--- config/config.php
+++ config/config.php
@@ -1,13 +1,17 @@
 <?php
 
+$database['login']= "Benchoco";
+$database['password']= "makougoum";
+$database['name']= "dbarvandamme";
 
-$rep = __DIR__ . '/../';
-//BD
-$base = "votre_base";
-$login = "";
-$mdp = "";
-//Vues
-$vues['erreur'] = 'vues/erreur.php';
-$vues['vuephp1'] = 'vues/vuephp1.php';
-
-?>
+$vues['erreur']= "vues/erreur.php";
+$vues['erreur']= "vues/pageAccueil.php";
+$vues['erreur']= "vues/Connexion.php";
+$vues['erreur']= "vues/erreur.php";
+$vues['erreur']= "vues/erreur.php";
+$vues['erreur']= "vues/erreur.php";
+$vues['erreur']= "vues/erreur.php";
+$vues['erreur']= "vues/erreur.php";
+$vues['erreur']= "vues/erreur.php";
+$vues['erreur']= "vues/erreur.php";
+$vues['erreur']= "vues/erreur.php";
--- controleur/Controleur.php
+++ controleur/Controleur.php
@@ -1,11 +1,16 @@
 <?php
 
+require_once("../config/Connection.php");
 
+$user= 'Benchoco';
+$pass='makougoum';
+$dsn='mysql:host=localhost;dbname=dbarvandamme';
 
 class Controleur
 {
     public function __construct(){
         //code du contrôleur ici
     }
+
 
 }
