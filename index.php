5187f505dbd12a8eae66a26f9e1506b2633cc5de
ZartaX0O3 <arthur.avd69@gmail.com>
2021-11-24 10:23:25 +0100

    Création base du projet

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>ToDo List Application PHP and MySQL</title>
+</head>
+<body>
+<div class="heading">
+    <h2>ToDo List Application PHP and MySQL database</h2>
+</div>
+<form method="post" action="index.php" class="input_form">
+    <label>
+        <input type="text" name="task" class="task_input">
+    </label>
+    <button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
+</form>
+</body>
+</html>
