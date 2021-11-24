715e689db206469b004aae77fb73a24df82168f3
ZartaX0O3 <arthur.avd69@gmail.com>
2021-11-24 11:03:43 +0100

    Création base du projet

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$rep = __DIR__ . '/../';
+//BD
+$base = "votre_base";
+$login = "";
+$mdp = "";
+//Vues
+$vues['erreur'] = 'vues/erreur.php';
+$vues['vuephp1'] = 'vues/vuephp1.php';
+
+?>
