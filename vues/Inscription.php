f8b2e97df99e602cb2238eafc40f3cc754b858bc
Ben <bengregorygbr@hotmail.com>
2021-12-04 18:47:42 +0100

    ajout d'une page d'inscription

---
+++ vues/Inscription.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>Inscription</title>
+    <link rel="stylesheet" href="../Styles/Inscription.css.css">
+</head>
+<body>
+
+
+<nav>
+
+    <div class="boutons">
+        <button>Se Connecter</button>
+        <button>S'inscrire</button>
+    </div>
+
+</nav>
+</body>
+</html>
+<?php
