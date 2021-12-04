92f05b4425624679295cd7ebd0c21b45e237c713
Ben <bengregorygbr@hotmail.com>
2021-12-04 18:47:25 +0100

    modification de la page d'accueil

--- vues/accueil.php
+++ vues/accueil.php
@@ -9,43 +9,29 @@
 <nav>
 
     <div class="boutons">
-        <button>Se Connecter</button>
+        <button type="submit"  > Se Connecter</button>
         <button>S'inscrire</button>
     </div>
 
 </nav>
 <h2 align="center">Accueil</h2>
 <form method="post" name="ajout-tache-pub" id="formTPub" align="center">
-    <?php
-
-        if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            $nom = $_POST["nom"];
-            $desc = $_POST["description"];
-            if($nom && $desc){
-                $tacheGw = new TacheGateway($con);
-                $tacheGw->ajouterTache($nom, $desc, 1);
-            }
-        }
-    ?>
     <label>Nom</label>
     <input name="nom" value="">
     <label>Description</label>
     <input name="description" value="">
-    <input type="submit"  name="action" value="ajouterTache"/>
+    <input type="submit"  name="action" value="ajouterTache" id="form-submit" align="center"/>
 </form>
 <?php
 
 
 try{
-    $query = "SELECT * FROM tache";
+        $tache = new Tache();
+        $tabTaches = $tache->voirTaches(1);
+        foreach($tabTaches as $value){
+            echo '<div class="tache">' . '<input type="checkbox"> ' . '<label>'  . $value["nom"] . " - ETAT :  " . $value["etat"]  .'</div>' . '<br>';
 
-    $con->executeQuery($query);
-    $results = $con->getResults();
-
-    foreach($results as $value){
-        echo '<div class="tache">' . '<input type="checkbox"> ' . '<label>'  . $value['nom'] . " - ETAT :  " . $value['etat']  .'</div>' . '<br>';
-    }
-
+        }
 }
 catch (Exception $e){
     echo $e;
