c9ab203ae41af1e7e79336ee9e3f3cf8582036e8
Ben <bengregorygbr@hotmail.com>
2021-12-15 10:47:10 +0100

    ajout FrontController.php

--- controleur/FrontController.php
+++ controleur/FrontController.php
@@ -2,7 +2,27 @@
 
 class FrontController
 {
-    public function __construct(){
 
+    public function __construct(){
+        global $vues;
+        session_start();
+
+        $listeActionAdmin = array("supprimerTachePb", "supprimerListePb", "supprimerUser");
+        if(in_array($action, $listeActionAdmin) == true){
+            if(isset($_SESSION) && ($_SESSION['role'] == 'admin')){
+                //connexion()
+                //isAdmin()
+            }
+        }
+
+
+
+        $listeActionUtilisateur = array("ajouterListePv", "ajouterTachePv", "");
+        if(in_array($action, $listeActionUtilisateur)){
+
+            else{
+                require $vues['accueil']; //remplacer accueil par connexion des que possible
+            }
+        }
     }
 }
