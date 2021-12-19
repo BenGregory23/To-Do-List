793d48605396b2cf23f0bfd3cc0bbf2d064c613b
Ben <bengregorygbr@hotmail.com>
2021-12-19 23:44:24 +0100

    modification de la page de connexion.php

---
+++ vues/connexion.php
@@ -0,0 +1,18 @@
+
+
+<html>
+<body>
+
+<div class="div-inscription">
+    <h2>Connexion</h2>
+    <form method="post" name="ajout-utilisateur" >
+        <label>Nom d'utilisateur</label>
+        <input type="text" name="login">
+        <label> Mot de passe</label>
+        <input type="password" name="mdp">
+        <input type="submit" value="Se connecter"/>
+        <input type="hidden" name="action" value="validerConnexion"/>
+    </form>
+</div>
+</body>
+</html>
