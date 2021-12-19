68fd62c975eb1de74f15887211beef1f680f0a57
Ben <bengregorygbr@hotmail.com>
2021-12-19 23:45:45 +0100

    modification formulaire Inscription.php

--- vues/Inscription.php
+++ vues/Inscription.php
@@ -1,39 +1,16 @@
 <html>
-<head>
-    <title>Inscription</title>
-    <link rel="stylesheet" href="../Styles/Inscription.css">
-</head>
 <body>
-
-
-<nav>
-
-    <div class="boutons">
-
-        <input type="submit"  > Se Connecter</input>
-        <button>S'inscrire</button>
-    </div>
-    <div class="titre">
-        <h1>TO DO LISTE</h1>
-    </div>
-    <div class="boutons1">
-        <input type="submit"  > Se Connecter</input>
-        <button>S'inscrire</button>
-    </div>
-
-
-</nav>
 
 <div class="div-inscription">
     <h2>Inscription</h2>
     <form method="post" name="ajout-utilisateur" >
         <label>Nom d'utilisateur</label>
-        <input type="text" name="nomUser">
+        <input type="text" name="login">
         <label> Mot de passe</label>
-        <input type="password" name="mdpUser">
-        <input type="submit" name="action" value="S'inscrire"
+        <input type="password" name="mdp">
+        <input type="submit" value="S'inscrire"/>
+        <input type="hidden" name="action" value="validerInscription"/>
     </form>
 </div>
 </body>
 </html>
-<?php
