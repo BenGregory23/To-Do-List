9c40f2d7da8fc7c62e177df3c0d6803c80c1fb5e
Ben <bengregorygbr@hotmail.com>
2021-12-19 23:42:09 +0100

    modification ControleurInvite.php

--- controleur/ControleurInvite.php
+++ controleur/ControleurInvite.php
@@ -7,26 +7,35 @@ class ControleurInvite
         global $rep, $vues;
         $dVueErreur = array();
 
-        $listeAction = array('ajouterTache', 'ajouterListe');
+        $listeAction = array('ajouterTachePb', 'ajouterListe', 'voirListePb');
         try {
             $action = $_REQUEST['action'];
             switch ($action) {
+                case 'Accueil':
                 case NULL :
                     $this->reinit();
                     break;
-                case 'ajouterTache' :
-                    $nom = $_REQUEST["nom"];
-                    $desc = $_REQUEST["description"];
-                    $idListe = $_REQUEST["idListe"];
-                    if($nom && $desc){
-                        $tache = new Tache($nom, $desc, $idListe, $_SESSION["pseudo"]);
-                        $tache->ajouterTache();
-                    }
-                    $this->reinit();
+                case 'Inscription' :
+                    require $vues['inscription'];
+                    break;
+                case "validerInscription" :
+                    $this->inscription();
+                    break;
+                case 'Connexion' :
+                    require $vues['connexion'];
+                    break;
+                case 'validerConnexion' :
+                    $this->connexion();
+                    break;
+                case 'ajouterTachePb' :
+                    $this->ajouterTachePb();
                     break;
 
+                case 'voirListePb':
+                    $this->voirTachesListe();
+                    break;
                 case 'ajouterListe' :
-
+                    break;
                 default :
                     echo 'DEFAULT';
             }
@@ -36,11 +45,62 @@ class ControleurInvite
         }
     }
 
+    public function connexion(){
+        global $rep, $vues, $dsn, $user, $mdp;
 
+        try{
+            $mdlUser = new MdlUtilisateur();
+            echo 'TEST';
+            $mdlUser->connexion($_REQUEST['login'], $_REQUEST['mdp']);
+        }
+        catch (Exception $e){
+            echo 'Erreur lors de la connexion' . $e . '<br>';
+        }
+
+    }
+
+    public function inscription(){
+        global $rep, $vues, $con, $dVueErreur;
+        try{
+            $userGw = new UserGateway($con);
+            $login = $_REQUEST["login"];
+            $mdp = $_REQUEST["mdp"];
+            $userGw->inscription($login, $mdp);
+        }catch(Exception $e){
+            $erreur = "Erreur lors de l'inscription" . $e;
+            $dVueErreur[] = $erreur;
+        }
+
+    }
+
+    public function ajouterTachePb(){
+        global $dVueErreur;
+        try{
+            $nom = $_REQUEST["nom"];
+            $desc = $_REQUEST["description"];
+            //$idListe = $_REQUEST["idListe"];
+            $idListe = 1;
+            if($nom && $desc) {
+                $tache = new MdlTache();
+                $tache->ajouterTache($nom, $desc, $idListe);
+            }
+        }
+        catch (Exception $e){
+            $erreur = "Erreur lors de l'ajout d'une tache publique" . $e;
+            $dVueErreur[] = $erreur;
+        }
+
+    }
+
+    public function voirTachesListe(){
+        global $vues;
+        require $vues['tachesListe'];
+    }
 
     public function reinit(){
         global $rep, $vues;
-        require ($vues['accueil']);
+        echo 'BITE';
+        require $vues['accueil'];
     }
 
 }
