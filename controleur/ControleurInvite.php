4f0b9e213691e81fd53a67d03b61c7e5595f7279
ZartaX0O3 <arthur.avd69@gmail.com>
2022-01-05 08:23:01 +0100

    oui

--- controleur/ControleurInvite.php
+++ controleur/ControleurInvite.php
@@ -1,140 +1,171 @@
 <?php
 
-use JetBrains\PhpStorm\NoReturn;
-
 class ControleurInvite
 {
-    public function __construct($action){
+    public function __construct($action)
+    {
 
         global $rep, $vues;
         $dVueErreur = array();
 
         try {
             switch ($action) {
+
                 case 'Accueil':
                 case NULL :
                     $this->accueil();
                     break;
+
                 case 'Inscription' :
                     $this->showInscription();
                     break;
+
                 case "validerInscription" :
                     $this->inscription();
                     break;
+
                 case 'Connexion' :
                     $this->showConnexion();
                     break;
+
                 case 'validerConnexion' :
                     $this->connexion();
                     break;
-                case 'ajouterTachePb' :
-                    $this->ajouterTachePb();
+
+                case 'ajouterTache' :
+                    $this->ajouterTache();
                     break;
-                case 'ajouterListePb' :
-                    $this->ajouterListePb();
+
+                case 'ajouterListe' :
+                    $this->ajouterListe();
                     break;
+
                 case 'voirListePb':
                     $this->voirTachesListe();
                     break;
-                case 'ajouterListe' :
-                    break;
+
                 default :
-                    require ($rep.$vues['accueil']);
+                    require($rep . $vues['accueil']);
                     break;
 
             }
-        }
-        catch (Exception $e)
-        {
-            $dVueErreur[] ="Error not expected";
-            require ($rep.$vues['accueil']);
+        } catch (Exception $e) {
+            $dVueErreur[] = "Error not expected";
+            require($rep . $vues['accueil']);
         }
         exit(0);
-    }
-
-    public function connexion(){
-        global $rep, $vues, $dsn, $user, $mdp;
-
-        try{
-            $mdlUser = new MdlUtilisateur();
-            echo 'TEST';
-            $mdlUser->connexion($_REQUEST['login'], $_REQUEST['mdp']);
-        }
-        catch (Exception $e){
-            echo 'Erreur lors de la connexion' . $e . '<br>';
-        }
-
     }
 
     /**
      * @throws Exception
      */
-    public function accueil(){
-        global $rep,$vues;
+    public function accueil()
+    {
+        global $rep, $vues;
 
         $mdlListe = new MdlListe();
         $tabListes = $mdlListe->mdlFindAllListes();
 
-        require ($rep.$vues['accueil']);
+        require($rep . $vues['accueil']);
     }
 
-    public function showInscription(){
+    public function showInscription()
+    {
         global $vues;
 
-        try{
+        try {
             require $vues["inscription"];
-        }catch (Exception $e){
+        } catch (Exception $e) {
             $erreur = "Erreur lors de l'affichage du formulaire d'inscription" . $e;
             $dVueErreur[] = $erreur;
         }
     }
 
-    public function inscription(){
+    public function inscription()
+    {
         global $rep, $vues, $con, $dVueErreur;
-        try{
+        try {
 
             $userGw = new UserGateway($con);
             $login = $_REQUEST["login"];
             $mdp = $_REQUEST["mdp"];
             $userGw->inscription($login, $mdp);
-        }catch(Exception $e){
+        } catch (Exception $e) {
             $erreur = "Erreur lors de l'inscription" . $e;
             $dVueErreur[] = $erreur;
         }
 
     }
 
-    public function showConnexion(){
+    public function showConnexion()
+    {
         global $vues;
 
-        try{
+        try {
             require $vues['connexion'];
-        }catch (Exception $e){
+        } catch (Exception $e) {
             $erreur = "Erreur lors de l'affichage du formulaire de connexion" . $e;
             $dVueErreur[] = $erreur;
         }
     }
 
-    public function ajouterTachePb(){
+    public function connexion()
+    {
+        global $rep, $vues, $dsn, $user, $mdp;
+
+        try {
+            $mdlUser = new MdlUtilisateur();
+            $mdlUser->connexion($_REQUEST['login'], $_REQUEST['mdp']);
+            require $vues["accueil"];
+        } catch (Exception $e) {
+            echo 'Erreur lors de la connexion' . $e . '<br>';
+        }
+
+    }
+
+    public function ajouterTache()
+    {
         global $dVueErreur;
-        try{
+        try {
             $nom = $_REQUEST["nom"];
             $desc = $_REQUEST["description"];
             $idListe = $_REQUEST["idListe"];
-            if($nom && $desc) {
+            if ($nom && $desc) {
                 $tache = new MdlTache();
                 $tache->ajouterTache($nom, $desc, $idListe);
-                echo 2;
             }
-        }
-        catch (Exception $e){
+        } catch (Exception $e) {
             $erreur = "Erreur lors de l'ajout d'une tache publique" . $e;
             $dVueErreur[] = $erreur;
         }
 
     }
 
-    public function voirTachesListe(){
+    private function ajouterListe()
+    {
+        global $dVueErreur;
+        try {
+            $nom = $_REQUEST["nom"];
+            $desc = $_REQUEST["description"];
+
+            if (isset( $_REQUEST["isPublic"])) $public = 1;
+            else $public = 0;
+
+            if($_SESSION['login'] != "") $user = $_SESSION['login'];
+            else $user = "Invité";
+
+            if ($nom && $desc) {
+                $liste = new MdlListe();
+                $liste->mdlAjouterListe($nom, $desc, $public, $user);
+            }
+        } catch (Exception $e) {
+            $erreur = "Erreur lors de l'ajout d'une tache publique" . $e;
+            $dVueErreur[] = $erreur;
+        }
+    }
+
+    public function voirTachesListe()
+    {
         global $vues, $idListe;
 
         $idListe = $_REQUEST['idListe'];
@@ -142,30 +173,14 @@ class ControleurInvite
         require $vues["tachesListe"];
     }
 
-    function voirListe(array $tabInfo, int ...$idListe){
+    function voirListe(array $tabInfo, int ...$idListe)
+    {
 
     }
 
-
-    public function reinit(){
+    public function reinit()
+    {
         global $rep, $vues;
         require_once $vues['accueil'];
-    }
-
-    private function ajouterListePb()
-    {
-        global $dVueErreur;
-        try{
-            $nom = $_REQUEST["nom"];
-            $desc = $_REQUEST["description"];
-            if($nom && $desc) {
-                $liste = new MdlListe();
-                $liste->mdlAjouterListe($nom, $desc);
-            }
-        }
-        catch (Exception $e){
-            $erreur = "Erreur lors de l'ajout d'une tache publique" . $e;
-            $dVueErreur[] = $erreur;
-        }
     }
 }
